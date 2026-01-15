4cd5a56026331e17535e7dcea06a9082d4790fc7
LalibThabib <luis.issermann@gmail.com>
2026-01-15 15:41:39 +0000

    Bacgkground link

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -8,7 +8,8 @@ function AdminShell({ children }: { children: React.ReactNode }) {
   const { rightContent } = useHeaderSlot();
 
   return (
-    <div className="min-h-screen bg-[#222222] text-[#e2e2e2] [background-image:url('/images/patterns/bg-pattern.svg')] [background-repeat:repeat] [background-size:400px_400px]">
+    <div className="min-h-screen bg-[#222222] text-[#e2e2e2] [background-image:url('/free-nextjs-admin-dashboard/images/patterns/bg-pattern.svg')]
+ [background-repeat:repeat] [background-size:400px_400px]">
       <AppHeader
         leftContent={<div className="text-sm font-semibold">Galactic Contracts</div>}
         rightContent={rightContent}
